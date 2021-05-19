2dc6caed2964de7229f38698339d29fd617c6a73
sasaki <a2014as@aiit.ac.jp>
2021-05-19 14:17:19 +0000

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,8 +3,9 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
-    @admin     = users(:michael)
+    @admin = users(:michael)
     @non_admin = users(:archer)
+    @non_activated_user = users(:hoge)
   end
 
   test "index as admin including pagination and delete links" do
@@ -12,21 +13,31 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    # 下記のUserにwhereメソッドを付加
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
+
+    # 有効化されていないアカウントのリンクが無いことを確認
+    assert_select 'a[href=?]', user_path(@non_activated_user), count: 0
+
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
     end
   end
-
+  
   test "index as non-admin" do
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+  
+  test "login as non-activated user" do
+    log_in_as(@non_activated_user)
+    assert_redirected_to root_url
   end
 end
